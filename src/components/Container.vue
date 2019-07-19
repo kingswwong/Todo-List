1f3e96f103526cb58f50bbf636ecdc25ca7b050f
“Dean” <467271883@qq.com>
2019-07-19 18:20:47 +0800

    Initial commit

---
+++ src/components/Container.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <p>Jquery Todo List</p>
+      <p>Simple Todo List with adding and filter by diff status</p>
+      <input type="text" v-model="todoMsg"><span><button @click="addTodoMsg">Add</button></span>
+      <ItemList ref='itemList'></ItemList>
+    </div>
+</template>
+
+<script>
+    import ItemList from "@/components/ItemList";
+    export default {
+        name: "Container",
+      components: {ItemList},
+      data(){
+          return{
+            todoMsg:""
+          }
+      },
+      methods:{
+          addTodoMsg(){
+            this.$refs.itemList.addMsg(this.todoMsg)
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-for="(item, index ) in itemList">
+      {{index + 1}} <input type="checkbox" v-model="item.isFinish"> {{item.content}}
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "ItemList",
+    data() {
+      return {
+        itemList: []
+      }
+    },
+    methods: {
+      addMsg(message) {
+        let item = {
+          isFinish: false,
+          content: message
+        }
+        this.itemList.push(item);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
